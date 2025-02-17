6a68caaf4cc7b24a323abf78351afaa30a153f3d
Aaron Gazzola <aaron@gazzola.dev>
2025-02-18 05:07:50 +1100

    cleanup(ProjectPage): remove fallback

--- app/[project_slug]/page.tsx
+++ app/[project_slug]/page.tsx
@@ -1,13 +1,6 @@
 "use client";
 
 import { ProjectPage } from "@/components/projects/ProjectPage";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import useDemoData from "@/hooks/useDemoData";
 
 interface ProjectPageProps {
@@ -18,26 +11,8 @@ interface ProjectPageProps {
 
 export default function ProjectOverviewPage({ params }: ProjectPageProps) {
   const { project } = useDemoData();
-  const getInvitesIsPending = false;
-
-  const hasInvite = false;
 
   const handleUpdate = () => {};
-
-  if (!hasInvite && !getInvitesIsPending)
-    return (
-      <Card>
-        <CardHeader>
-          <CardTitle>Project Not Found</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <CardDescription>
-            The project you are looking for does not exist or you do not have
-            access to it.
-          </CardDescription>
-        </CardContent>
-      </Card>
-    );
 
   return <ProjectPage projectData={project} onUpdate={handleUpdate} />;
 }
